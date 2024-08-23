333a69ca19d2e4b1790548e12dda2f51ae276161
= <=>
2024-08-22 23:57:02 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [] do
+      resources :customers do
         resources :subscriptions, only: [:create, :update, :index]
       end
     end
